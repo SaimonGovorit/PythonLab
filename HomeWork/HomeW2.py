ce15ae948227bc28ccf592e06b5a3b806bc5a322
SaimonGovorit <blacksemen1@gmail.com>
2021-10-18 23:27:06 +0300

    HomeW2.py

--- HomeWork/HomeW2.py
+++ HomeWork/HomeW2.py
@@ -1,1 +1,31 @@
-print("Hello")
+#Задание 1
+num1 = 2
+num2 = 5
+num3 = 10
+
+print(num1 + num2 + num3)
+print(num1 * num2 * num3)
+
+# Задание 2
+
+nim1 = 10000
+nim2 = 3500
+nim3 = 850
+x = nim1 - nim2 - nim3
+print(x)
+
+# Задание 3
+
+side_a = int(input("Размер стороны a:"))
+side_b = int(input("Размер стороны b:"))
+
+square = (side_a**2 * side_b**2)
+print(square)
+
+#  Задание 4
+
+print("To be\nor not\nto be")
+
+# Задание 5
+
+print("Life is what happens\n     when\n           you're busy making other plans\n                                    John Lennon.")
